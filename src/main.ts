df64f69ec435af7dd2150e7161e71ebce95d9d65
Aetstapa <aetstapa@outlook.com>
2026-02-22 18:38:17 +0800

    fix npm

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { corgi } from '.';
 
 document.getElementById('info')?.addEventListener('click', () => {
-    corgi.bark('hello', { live: 0 });
+    corgi.info('hello', { live: 0 });
 });
 
 document.getElementById('success')?.addEventListener('click', () => {
-    corgi.yip('success', { live: 0 });
+    corgi.success('success', { live: 0 });
 });
 
 document.getElementById('warn')?.addEventListener('click', () => {
-    corgi.gruff('This is warning message', { title: 'warning!!!' });
+    corgi.warn('This is warning message', { title: 'warning!!!' });
 });
 
 document.getElementById('error')?.addEventListener('click', () => {
-    corgi.snarl({ html: '<a href="">error</a>' });
+    corgi.error({ html: '<a href="">error</a>' });
 });
